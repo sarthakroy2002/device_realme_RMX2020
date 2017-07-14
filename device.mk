d5a6e985b634a755aedb7dd6dbfe955a91896890
Jerry Zhang <zhangjerry@google.com>
2017-07-14 11:36:32 -0700

    rmx2020: rootdir: Set sys.usb.ffs.aio_compat
    The kernel does not support aio with ffs.
    Set the property so adb/mtp know to use the compatible
    functions.
    
    Bug: 37916658
    Test: Use adb / use adb in recovery
    Change-Id: Id35a05fcb9952dcf7239bd15a61cc620537419bc
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Change-Id: I45ce6b618d4f4523b6cda7a0900faf8e90e8df05

--- device.mk
+++ device.mk
@@ -43,6 +43,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/etc/fstab.mt6768:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6768
 
+# Init
+PRODUCT_PACKAGES += \
+    init.mt6768.rc
+
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/idc/mtk-kpd.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/mtk-kpd.idc \
