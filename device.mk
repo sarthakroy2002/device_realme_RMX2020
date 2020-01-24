f756afdddb4a98fe3bd964401f3c0d93a358eada
Arseniy Graur <argraur@gmail.com>
2020-01-24 14:08:14 +0000

    RMX2020: Use Pixel offline charging animation
    Change-Id: Ibcad06a3cef4280dd3193cb6cb3f0886ea92e564

--- device.mk
+++ device.mk
@@ -300,6 +300,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Pixel Charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0 \
