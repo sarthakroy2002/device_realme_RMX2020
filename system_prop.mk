f24daa330a82eb10f0be302a45f2da17027cecd0
Lucas Dupin <dupin@google.com>
2020-03-14 15:01:44 -0700

    RMX2020: Disable blur by default
    Change-Id: If28a2b702597a7bfc331ae77dcb2e3f4ec07d3fc
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -1,6 +1,8 @@
 # Blurs
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1
+    ro.sf.blurs_are_expensive=1 \
+    persist.sys.sf.disable_blurs=1
 
 # Camera
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
