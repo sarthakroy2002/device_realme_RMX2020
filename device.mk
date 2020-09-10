bb24e928c7f4767e91dd02a99fae088296c0548b
Davide Garberi <dade.garberi@gmail.com>
2020-09-10 03:57:15 +0200

    rmx2020: Build libhidltransport and libhwbinder
    Needed for older blobs as we doesn't build vendor partition
    
    Change-Id: I8c503b7847beac6875ab289ef2acadfcb6d1ee2c
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- device.mk
+++ device.mk
@@ -59,6 +59,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhwbinder
+
 # Init
 PRODUCT_PACKAGES += \
     init.mt6768.rc \
