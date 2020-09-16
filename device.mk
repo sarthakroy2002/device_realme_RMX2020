8266b966cac57c00cfa6a40bbbf44df27ca38943
Peter Cai <peter@typeblog.net>
2020-09-16 22:50:30 +0200

    rmx2020:Initial skeleton tree
    Co-authored-by: TheMalachite <eliasgheeraert@gmail.com>
    Change-Id: I0a77c525062cdaaf9b9c2233c699b04c26d52282
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

---
+++ device.mk
@@ -0,0 +1,53 @@
+#
+# Copyright (C) 2020 Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/realme/rmx2020
+
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Dynamic Partition
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_BUILD_SUPER_PARTITION := false
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default
+ 
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/rootdir/etc/fstab.mt6768:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6768
+
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/idc/mtk-kpd.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/mtk-kpd.idc \
+    $(DEVICE_PATH)/keylayout/mtk-kpd.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/mtk-kpd.kl
+
+# Screen density
+PRODUCT_AAPT_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
---
+++ lineage_rmx2020.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2020 Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile
+$(call inherit-product, device/realme/rmx2020/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_rmx2020
+PRODUCT_DEVICE := rmx2020
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := Realme G70 Series
+PRODUCT_MANUFACTURER := realme
+
+# Build info
+BUILD_FINGERPRINT := "realme/RMX2027/RMX2027:10/QP1A.190711.020/1604480355:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=rmx2020 \
+    PRODUCT_NAME=rmx2020 \
+    PRIVATE_BUILD_DESC="full_oppo6769-user 10 QP1A.190711.020 68b77aba7cb33275 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
