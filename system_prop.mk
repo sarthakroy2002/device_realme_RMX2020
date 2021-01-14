e0f176252b3fc41ae91db4bc8f4a9c97811c1655
zjunior06 <zjunior06@pixelexperience.org>
2021-01-14 12:46:52 -0300

    RMX2020: Add some props for better ram management
    Change-Id: I3ab7d8546086451c5bb571d949885f4ea6c9aac3

--- system_prop.mk
+++ system_prop.mk
@@ -87,3 +87,14 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.device_config.runtime_native.usap_pool_enabled=true
 	
+# Tweak the memory management of the device, enable more background apps.. etc..
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.fha_enable=true \
+    ro.sys.fw.bg_apps_limit=32 \
+    ro.config.dha_cached_max=16 \
+    ro.config.dha_empty_max=42 \
+    ro.config.dha_empty_init=32 \
+    ro.config.dha_lmk_scale=0.545 \
+    ro.config.dha_th_rate=2.3 \
+    ro.config.sdha_apps_bg_max=64 \
+    ro.config.sdha_apps_bg_min=8
