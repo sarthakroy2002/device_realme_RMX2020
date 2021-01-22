0ed4990713e40b542e15e13fece69b64d52d9b42
Inder <ishantpandey9@gmail.com>
2021-01-22 14:57:40 +0530

    rmx2020 : device.mk:Define proprietary blobs
    Change-Id: Ib6a34c9db8a98ba9bb9a4f618e370c7c0900e267

--- device.mk
+++ device.mk
@@ -19,6 +19,9 @@ DEVICE_PATH := device/realme/rmx2020
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Call proprietary blob setup
+$(call inherit-product-if-exists, vendor/realme/rmx2020/rmx2020-vendor.mk)
+
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
