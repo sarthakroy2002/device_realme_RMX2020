13d5208f418dfe81d475e622a08646d9b80e6a11
Inder <ishantpandey9@gmail.com>
2021-01-22 15:19:50 +0530

    rmx2020: Move props definition to product makefile and Define perf and surfaceflinger props
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Change-Id: I67470ee18bd312152c18a3ef1d63a3e9fd6b3ce3

--- system_prop.mk
+++ system_prop.mk
@@ -2,3 +2,15 @@
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     debug.stagefright.omx_default_rank.sw-audio=1 \
     debug.stagefright.omx_default_rank=0
+    debug.stagefright.ccodec=0
+
+# Performance
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.mtk_perf_simple_start_win=1 \
+    ro.mtk_perf_fast_start_win=1 \
+    ro.mtk_perf_response_time=1
+
+# Surfaceflinger
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    debug.sf.latch_unsignaled=1
+
