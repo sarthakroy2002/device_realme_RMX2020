3c3701ea325fb29cb65339948aadc55a148874cc
Utkarsh Kanojiya <48874181+iamlordutkarsh@users.noreply.github.com>
2021-01-27 20:05:13 +0530

    RMX2020: Fix Typo
    Change-Id: Ic045de13d4ff4f68667fce929332e94052f51773

--- device.mk
+++ device.mk
@@ -45,7 +45,7 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     audio.a2dp.default
 
-#PRODUCT_COPY_FILES += \
+PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_EXTRA_VNDK_VERSIONS)/etc/audio_policy_configuration.xml
 
 # fastbootd
