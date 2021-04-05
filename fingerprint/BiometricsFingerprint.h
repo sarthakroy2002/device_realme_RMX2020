250958b2eaf3cb5a9511a6e9b37883a83afab6f2
SamarV-121 <samarvispute121@gmail.com>
2021-04-05 14:26:54 +0530

    RMX2020: {fingerprint,interfaces}: oppo -> oplus
    Signed-off-by: Sarthak <sarthakroy2002@gmail.com>

--- fingerprint/BiometricsFingerprint.h
+++ fingerprint/BiometricsFingerprint.h
@@ -25,7 +25,7 @@
 #include <hidl/Status.h>
 #include <android/hardware/biometrics/fingerprint/2.1/IBiometricsFingerprint.h>
 #include <android/hardware/biometrics/fingerprint/2.1/types.h>
-#include <vendor/oppo/hardware/biometrics/fingerprint/2.1/IBiometricsFingerprint.h>
+#include <vendor/oplus/hardware/biometrics/fingerprint/2.1/IBiometricsFingerprint.h>
 
 namespace android {
 namespace hardware {
@@ -45,7 +45,7 @@ using ::android::OK;
 using ::android::sp;
 using ::android::status_t;
 
-class OppoClientCallback;
+class OplusClientCallback;
 struct BiometricsFingerprint : public IBiometricsFingerprint {
 public:
     BiometricsFingerprint();
@@ -73,9 +73,9 @@ public:
     Return<RequestStatus> getEngineeringInfo(uint32_t type);
 
 private:
-    sp<vendor::oppo::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint> mOppoBiometricsFingerprint;
-    sp<OppoClientCallback> mOppoClientCallback;
-    static Return<RequestStatus> OppoToAOSPRequestStatus(vendor::oppo::hardware::biometrics::fingerprint::V2_1::RequestStatus req);
+    sp<vendor::oplus::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint> mOplusBiometricsFingerprint;
+    sp<OplusClientCallback> mOplusClientCallback;
+    static Return<RequestStatus> OplusToAOSPRequestStatus(vendor::oplus::hardware::biometrics::fingerprint::V2_1::RequestStatus req);
 };
 
 }  // namespace implementation
