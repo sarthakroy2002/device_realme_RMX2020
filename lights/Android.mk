2a1dc5558a954000091ed0ebbd1a4a7d0db792ae
ManshuTyagi <himanshut567@gmail.com>
2021-04-08 23:30:51 +0530

    RMX2020: Drop unwanted policies and denials
    Signed-off-by: ManshuTyagi "himanshut567@gmail.com"
    Change-Id: Ie0a1eec8b964f775c4a92b315a5c1a02ab319adb

--- lights/Android.mk
+++ lights/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := android.hardware.light@2.0-service.RMX2001
+LOCAL_MODULE := android.hardware.light@2.0-service.RMX2020
 LOCAL_MODULE_STEM := android.hardware.light@2.0-service-mediatek
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_EXTRA_VNDK_VERSIONS)/bin
 LOCAL_MODULE_RELATIVE_PATH := hw
