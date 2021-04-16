9738d5f5eba84d74f3f1d802e0396ebb3f6aab7e
Sarthak <sarthakroy2002@gmail.com>
2021-04-16 11:40:51 +0530

    Revert "RMX2020: Copy hotword permission to product"
    This reverts commit 095a97f6a4472ee7eae94d8369cdeb980dc27cdb.
    
    Change-Id: I33cbd5accab1b7f7fd24ca126e7ca79763cd652e

--- system_prop.mk
+++ system_prop.mk
@@ -66,6 +66,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.mtk_perf_fast_start_win=1 \
     ro.mtk_perf_response_time=1
 
+# Privapp permissions whitelisting
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.control_privapp_permissions=log
+
 # Surfaceflinger
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     debug.sf.latch_unsignaled=1 \
