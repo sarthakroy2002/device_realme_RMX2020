2ecfadedc3d1ba472c3404bd6b5da8a86ff76973
imranpopz <imranpopz32@gmail.com>
2021-07-17 13:15:39 +0000

    RMX2020 : Build RenderScript HIDL hal
    Change-Id: Ic3c24e870443ab8bbc960f1bcd92702425635747

--- device.mk
+++ device.mk
@@ -220,6 +220,10 @@ PRODUCT_PACKAGES += \
     PresencePolling \
     RcsService
 
+# RenderScript
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/seccomp/configstore@1.1.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/configstore@1.1.policy \
