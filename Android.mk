0b7dc72fd9d9fbdaceb97f70ba95c5e491714c75
Erfan Abdi <erfangplus@gmail.com>
2021-07-21 14:53:25 +0000

    RMX2020: Add Gatekeeper symlinks
    - Based over GS290-dev/android_device_gigaset_GS290-unified@348b6ac
    
    Co-authored-by: TheMalachite <eliasgheeraert@gmail.com>
    Co-authored-by: Erfan Abdi <erfangplus@gmail.com>
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- Android.mk
+++ Android.mk
@@ -19,7 +19,23 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),RMX2020)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
+$(GATEKEEPER_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libSoftGatekeeper.so $@
+
 ALL_DEFAULT_INSTALLED_MODULES += $(GATEKEEPER_SYMLINK)
+
+GATEKEEPER_TRUSTONIC_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.mt6768.so
+GATEKEEPER_TRUSTONIC_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.oppo6769.so
+GATEKEEPER_TRUSTONIC_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.mt6768.so
+GATEKEEPER_TRUSTONIC_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.oppo6769.so
+$(GATEKEEPER_TRUSTKERNEL_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libMcGatekeeper.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GATEKEEPER_TRUSTONIC_SYMLINK)
 
 VULKAN_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/vulkan.mt6768.so
 VULKAN_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.mt6768.so
