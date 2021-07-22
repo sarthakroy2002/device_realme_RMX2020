ad98a0a4c0b06b73950bff8b19951f15fe227453
TheMalachite <eliasgheeraert@gmail.com>
2021-07-22 19:43:09 +0000

    RMX2020: Build libldacBT_dec from source
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -67,6 +67,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    libldacBT_dec
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
