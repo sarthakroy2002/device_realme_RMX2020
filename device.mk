8c54ce9a4f0050cb99233dadea0f1143a6ab1888
Ash Blake <36798666+ashblk@users.noreply.github.com>
2021-07-25 14:26:06 +0000

    RMX2020: build radio interfaces from source
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Ifeb56fbddfaa48fe53d07c327f25242a681b0323

--- device.mk
+++ device.mk
@@ -240,6 +240,20 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Radio
+PRODUCT_PACKAGES += \
+    android.hardware.broadcastradio@1.0 \
+    android.hardware.broadcastradio@1.1 \
+    android.hardware.radio@1.0 \
+    android.hardware.radio@1.1 \
+    android.hardware.radio@1.2 \
+    android.hardware.radio@1.3 \
+    android.hardware.radio@1.4 \
+    android.hardware.radio@1.5 \
+    android.hardware.radio.config@1.1 \
+    android.hardware.radio.config@1.2 \
+    android.hardware.radio.deprecated@1.0
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     engineer_vendor_shell.sh \
