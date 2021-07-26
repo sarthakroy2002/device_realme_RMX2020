86e0a4dd2fa4e55570ed69ac8ba0b34ea716f702
Kshitij Gupta <kshitijgm@gmail.com>
2021-07-26 19:02:40 +0530

    RMX2020: Build system android.hidl.[base,manager]@1.0

--- device.mk
+++ device.mk
@@ -120,7 +120,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.allocator@1.0 \
     android.hidl.base@1.0 \
+    android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_system \
     libhidltransport \
     libhwbinder \
     libhwbinder.vendor \
