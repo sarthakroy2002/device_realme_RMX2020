9696574270dd0ab035307065908d214a87b1303a
Zinadin Zidan <zidan44@pixelexperience.org>
2021-07-26 20:22:58 +0530

    RMX2020: configs: Use Prebuild APNS Config
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Ie5eb942ad4f7c61c205d89ff8a5e61fd95ab6bdd
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -75,6 +75,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# APN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     libldacBT_dec \
