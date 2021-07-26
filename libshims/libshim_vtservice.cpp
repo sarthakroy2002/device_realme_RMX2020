131f4bcfe2d377e0df9e39825ac32a086018ca12
SamarV-121 <samarvispute121@gmail.com>
2021-07-26 20:49:26 +0530

    RMX2020: Shim another missing libsink symbol
    * F/linker  (10501): CANNOT LINK EXECUTABLE "/system/bin/vtservice": cannot locate symbol "_ZN7android11AudioSourceC1E14audio_source_tRKNS_8String16Ejjjjii28audio_microphone_direction_tf" referenced by "/system/lib/libsink.so"...
    * Just provide an empty function
    
    Change-Id: I99bb17cac03314b98f84a04ed059abcfe29079bc
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- libshims/libshim_vtservice.cpp
+++ libshims/libshim_vtservice.cpp
@@ -1,3 +1,4 @@
- extern "C" {
+extern "C" {
    void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_output_flags_tPFviPvS4_ES4_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tjiPK18audio_attributes_tbfi() {}
+   void _ZN7android11AudioSourceC1E14audio_source_tRKNS_8String16Ejjjjii28audio_microphone_direction_tf() {}
 }
