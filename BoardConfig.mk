06923e4d15d307e74132c611b2a677a5fbfe0a1f
Sarthak Roy <sarthakroy2002@gmail.com>
2021-07-29 19:38:41 +0530

    RMX2020: Setup product prop and move some props to it
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -152,6 +152,7 @@ TARGET_TAP_TO_WAKE_NODE := /proc/touchpanel/double_tap_enable
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 
 # Recovery
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
