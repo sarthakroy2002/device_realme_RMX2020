93b3a00f22c50668b18768dbf12a1e2dc26df893
Sarthak Roy <sarthakroy2002@gmail.com>
2021-07-29 19:42:05 +0530

    RMX2020: Lets add back Realme Parts
    It is needed to fix USB OTG and DT2W
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -31,6 +31,9 @@ $(call inherit-product, vendor/mediatek/ims/mtk-ims.mk)
 # RealmeDirac
 $(call inherit-product, packages/apps/RealmeDirac/dirac.mk)
 
+# Parts
+$(call inherit-product-if-exists, packages/apps/RealmeParts/parts.mk)
+
 PRODUCT_SHIPPING_API_LEVEL := 29
 
 # VNDK
