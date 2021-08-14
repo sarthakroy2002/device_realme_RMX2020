aecd32b5559778a186fb6f694827c5a6a787d5c4
Zinadin Zidan <zidan44@pixelexperience.org>
2021-08-14 15:50:59 +0530

    RMX2020: media: Build media Codec2 modules
    * https://source.android.com/devices/media/updatable-media
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Ied9574bce55d1c27ef48810dfd07a9e2929be912

--- device.mk
+++ device.mk
@@ -179,6 +179,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Media Codec2 modules
+PRODUCT_PACKAGES += \
+    com.android.media.swcodec \
+    libsfplugin_ccodec
+
 # MTK-IMS
 PRODUCT_BOOT_JARS += \
     mediatek-common \
