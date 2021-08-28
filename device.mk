b6f79316602961e4ca5036d26c8541677524e5a5
Sarthak Roy <sarthakroy2002@gmail.com>
2021-08-28 20:57:25 +0530

    RMX2020: Build android.hardware.thermal@1.0 from source
    Change-Id: Ia54b077b285e0eb9fb55c1d9f5ead20ff31bd109
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -362,6 +362,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtextclassifier_hash.vendor
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 # TinyXML
 PRODUCT_PACKAGES += \
     libtinyxml
