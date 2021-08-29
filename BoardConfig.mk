6582714b4e24e7a53d9b3c46a6e3e05a14f9fe5b
Sarthak Roy <sarthakroy2002@gmail.com>
2021-08-29 23:02:45 +0530

    RMX2020: Switch to Permissive [TEMP]
    Change-Id: Id9a209982a672d1ca809addb0a8ba4b859acd8ed
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,7 +77,7 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # Kernel
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
-
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_KERNEL_PAGESIZE := 2048
