5a34a8a7b655c8b87f2eb0de9559b98d61873c16
Sarthak Roy <sarthakroy2002@gmail.com>
2021-09-06 10:48:07 +0530

    RMX2020: rootdir: Checkout changes from RMX2020_11_C.05
    * alps/sys_oplus_mssi_64_cn/oplus_mssi_64_cn:11/RP1A.200720.011/1607914664672:user/release-keys
    
    Change-Id: I03d8dc8fbce3003e4b55ca8f688d8f5cad4506d9
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -144,22 +144,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.oppo.reserve.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.oppo.reserve.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.oppo.vendor.motor.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.oppo.vendor.motor.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.project.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
