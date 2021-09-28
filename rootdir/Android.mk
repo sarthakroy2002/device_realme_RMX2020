215212daca8655b9d8d3e7231655ddbb6d0dd7ce
TheMalachite <eliasgheeraert@gmail.com>
2021-09-28 19:51:50 +0530

    RMX2020: Clean and re-arrange the fstab
    - Fixed indentation to improve readbility
    - Squashed swap specific fstab into generic one
    
    Change-Id: Iaf0d2a020a5cefc8f34f89ee4bff159c850381be
    Co-authored-by: Aayush Gupta <aayushgupta219@gmail.com>
    Co-authored-by: Debayan Kar <kardebayan3@gmail.com>
    Co-authored-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -216,14 +216,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.enableswap
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.enableswap
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.mt6768
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
