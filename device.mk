56b031ed342e31ba07f3acc1310830035b1c508d
maitreya29 <maitreyapatni30@gmail.com>
2021-10-08 12:22:49 +0530

    RMX2020: Build vendor variant of android.hardware.radio.deprecated
    Change-Id: If2357945f4ae06367b5dd1d0b5d4a4fcda7e9a2a
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -293,7 +293,8 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.5 \
     android.hardware.radio.config@1.1 \
     android.hardware.radio.config@1.2 \
-    android.hardware.radio.deprecated@1.0
+    android.hardware.radio.deprecated@1.0 \
+    android.hardware.radio.deprecated@1.0.vendor
 
 # Ramdisk
 PRODUCT_PACKAGES += \
