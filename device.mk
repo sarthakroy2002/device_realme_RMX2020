d4ec975997b8237b7577d1f9aa8ef8b142bbaa5d
Sarthak Roy <sarthakroy2002@gmail.com>
2021-10-08 12:29:29 +0530

    RMX2020: Build vendor variant of gatekeeper 1.0
    Change-Id: I3460ac072627c078ce5bc654d63eff7e75c9608b
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -124,6 +124,7 @@ PRODUCT_PACKAGES += \
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
+    android.hardware.gatekeeper@1.0.vendor \
     android.hardware.gatekeeper@1.0-impl
 
 # Health
