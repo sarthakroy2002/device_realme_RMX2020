a407ef17cdb9ec1214bd61c0cc7ebe40f012a683
Sarthak Roy <sarthakroy2002@gmail.com>
2021-10-08 21:43:57 +0530

    RMX2020: Build vendor variant of android.hidl.allocator@1.0
    10-08 10:32:50.644  1191  1191 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.trustonic.tee@1.0-service": library "android.hidl.allocator@1.0.so" not found: needed by main executable
    
    10-08 10:32:50.648  1192  1192 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.trustonic.teeregistry@1.0-service": library "android.hidl.allocator@1.0.so" not found: needed by main executable
    
    Change-Id: Icf4be85fc8764bc8625280c5df2b167280122993
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -144,6 +144,7 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.allocator@1.0 \
+    android.hidl.allocator@1.0.vendor \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
