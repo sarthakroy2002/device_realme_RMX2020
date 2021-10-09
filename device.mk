42c27059e7c01d64ec143274a76cbe1ec1ceaeb1
Sarthak Roy <sarthakroy2002@gmail.com>
2021-10-09 14:04:33 +0530

    RMX2020: Build android.hardware.drm@1.3
    10-09 08:05:28.046   664   664 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.drm@1.3-service.widevine": library "android.hardware.drm@1.3.so" not found: needed by main executable
    
    Change-Id: Id585cda439428f40e1df34c0cdbff544fe2fa454
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -124,6 +124,8 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2 \
     android.hardware.drm@1.2.vendor \
+    android.hardware.drm@1.3 \
+    android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.3-service.clearkey
 
 # fastbootd
