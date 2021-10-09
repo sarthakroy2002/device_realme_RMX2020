5d201a5696787d54aabb10b32785fcaf0c818be4
Sarthak Roy <sarthakroy2002@gmail.com>
2021-10-09 10:55:59 +0530

    RMX2020: Build android.hardware.bluetooth.a2dp
    01-01 05:47:32.365   655   655 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.audio@5.0-service-mediatek": library "android.hardware.bluetooth.a2dp@1.0.so" not found: needed by main executable
    
    Change-Id: I3435064bffd072a581d1aacdb0de98bd033747e4
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -92,7 +92,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libldacBT_dec \
     libldacBT_bco \
+    android.hardware.bluetooth.a2dp@1.0 \
     android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth.a2dp@1.0.vendor \
     libbtconfigstore
 
 # Camera
