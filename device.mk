5dd47c3a5746c4233d99be76c9602fff603a1b2e
Sarthak Roy <sarthakroy2002@gmail.com>
2021-10-09 09:29:37 +0530

    RMX2020: Build android.hardware.radio.config@1.0
    01-01 05:47:35.301  1150  1150 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/mtkfusionrild": library "android.hardware.radio.config@1.0.so" not found: needed by /vendor/lib64/librilfusion.so in namespace (default)
    
    Change-Id: I92d1f10dfff35e0452afbdddcae43dfb64e1bb30
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -323,6 +323,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.4.vendor \
     android.hardware.radio@1.5 \
     android.hardware.radio@1.5.vendor \
+    android.hardware.radio.config@1.0 \
     android.hardware.radio.config@1.1 \
     android.hardware.radio.config@1.2 \
     android.hardware.radio.deprecated@1.0 \
