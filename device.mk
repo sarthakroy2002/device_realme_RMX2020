689e08d0d1ec8a4a595a7c76ebbdfcd44d536ca5
Sarthak Roy <sarthakroy2002@gmail.com>
2021-10-09 17:56:13 +0530

    RMX2020: Build few wifi interfaces
    Fixes:
    10-08 09:18:21.171  2511  2511 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.wifi@1.0-service-lazy-mediatek": library "android.hardware.wifi@1.0.so" not found: needed by main executable
    
    Change-Id: I6b56d91b9d6f2a89d469ffa1dbecac13343bdfff
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -492,6 +492,14 @@ PRODUCT_PACKAGES += \
     WifiOverlay
 
 PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0 \
+    android.hardware.wifi@1.0.vendor \
+    android.hardware.wifi@1.1 \
+    android.hardware.wifi@1.1.vendor \
+    android.hardware.wifi@1.2 \
+    android.hardware.wifi@1.2.vendor \
+    android.hardware.wifi@1.3 \
+    android.hardware.wifi@1.3.vendor \
     libkeystore-engine-wifi-hidl \
     libkeystore-wifi-hidl \
 
