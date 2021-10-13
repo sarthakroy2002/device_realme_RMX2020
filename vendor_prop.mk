3027a579e44ccf0d985416c945edcc25649105f5
Sarthak Roy <sarthakroy2002@gmail.com>
2021-10-13 11:48:32 +0530

    RMX2020: Remove duplicated ro.sf.lcd_density prop
    Change-Id: Icac07cddfd405d27c9dce33913d2de4216463809
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -182,7 +182,6 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
     ro.mtk_cam_stereo_camera_support=1 \
     ro.opengles.version=196610 \
     ro.oppo.theme.version=7000 \
-    ro.sf.lcd_density=320 \
     ro.surface_flinger.primary_display_orientation=0 \
     ro.telephony.default_network=9,9,9,9 \
     ro.telephony.iwlan_operation_mode=default \
