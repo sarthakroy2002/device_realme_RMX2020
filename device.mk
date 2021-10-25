cc55b971ad4640ec526d3ec6e7b02d9c326ef46e
daniml3 <danimoral1001@gmail.com>
2021-10-25 20:42:36 +0530

    RMX2020: build android.hardware.vibrator-V1-ndk_platform.vendor
    Fixes:
    10-25 14:36:38.513   665   665 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.vibrator-service.mediatek": library "android.hardware.vibrator-V1-ndk_platform.so" not found: needed by main executable
    
    Change-Id: Ie01908a26dca5c9e54f8bc405aae604de0a5589e

--- device.mk
+++ device.mk
@@ -485,6 +485,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
+    android.hardware.vibrator-V1-ndk_platform.vendor \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
