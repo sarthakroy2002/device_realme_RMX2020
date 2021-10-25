0f709c83905586d3cddbad621059d33d657e6245
Sarthak Roy <sarthakroy2002@gmail.com>
2021-10-25 22:38:31 +0530

    RMX2020: Build android.hardware.camera.provider@2.6
    Fixes:
    10-25 16:33:37.539   873   873 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/camerahalserver": library "android.hardware.camera.provider@2.6.so" not found: needed by main executable
    
    Change-Id: Idf25d55875506bb44ddee695c219b34d56d62902
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -118,6 +118,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.5.vendor \
     android.hardware.camera.provider@2.5 \
     android.hardware.camera.provider@2.5.vendor \
+    android.hardware.camera.provider@2.6 \
+    android.hardware.camera.provider@2.6.vendor \
     android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4.vendor
 
