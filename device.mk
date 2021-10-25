14864890d3aad15b1b39d53a3e89c940fc627e80
Sarthak Roy <sarthakroy2002@gmail.com>
2021-10-25 20:50:04 +0530

    RMX2020: Build android.hardware.power-V1-ndk_platform.vendor
    Fixes:
    10-25 14:36:43.476  1273  1273 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service": library "android.hardware.power-V1-ndk_platform.so" not found: needed by main executable
    
    Change-Id: I1bea7fc0a7129d6061b9ae0c83a09d497f002976
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -328,6 +328,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power-V1-ndk_platform.vendor \
     android.hardware.power@1.0 \
     android.hardware.power@1.0.vendor \
     android.hardware.power@1.1 \
