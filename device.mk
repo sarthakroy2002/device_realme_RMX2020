be7e0837e478c54f8a7ae8de89b43ff66d6d4263
Sarthak Roy <sarthakroy2002@gmail.com>
2021-10-25 20:44:43 +0530

    RMX2020: Build android.hardware.thermal@2.0
    Fixes:
    10-25 14:36:38.457   655   655 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.thermal@2.0-service.mtk": library "android.hardware.thermal@2.0.so" not found: needed by main executable
    
    Change-Id: I1aaefdf58267088abeee93790c3c9aba0af716b1
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -469,6 +469,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0.vendor \
     android.hardware.thermal@2.0-impl \
     android.hardware.thermal@2.0-service \
     android.hardware.thermal@1.0-impl \
