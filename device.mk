a8954b9e35bc170205a4fcc2606c53e9b59fb285
Sarthak Roy <sarthakroy2002@gmail.com>
2021-11-10 10:54:33 +0530

    RMX2020: Remove all MTK common bootjars
    - Broken with Android 12 Build system, need to put these jars in a separate repo without soong namespace
    
    Change-Id: Ie22c6973d1a37ab48ff2a105dfc89c5376cafe45
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -227,16 +227,6 @@ PRODUCT_PACKAGES += \
     com.android.media.swcodec \
     libsfplugin_ccodec
 
-# MTK-IMS
-PRODUCT_BOOT_JARS += \
-    mediatek-common \
-    mediatek-framework \
-    mediatek-ims-base \
-    mediatek-ims-common \
-    mediatek-telecom-common \
-    mediatek-telephony-base \
-    mediatek-telephony-common
-
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
 
