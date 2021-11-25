9dd9b07891373a58a9e5b687e19d48b8987eabea
Vaisakh Murali <mvaisakh@statixos.com>
2021-11-25 10:31:05 +0530

    RMX2020: Build libxml2 from source
    Change-Id: Ia5341b3b6ddf5a31c87a9c80b64a5cb43277b704

--- device.mk
+++ device.mk
@@ -209,6 +209,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libsuspend
 
+# Libxml2
+PRODUCT_PACKAGES += \
+    libxml2.vendor
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.RMX2020
