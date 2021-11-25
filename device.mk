4bc2b44a18e91651f1a6a6ad32dcc556bce35c2b
Vaisakh Murali <mvaisakh@statixos.com>
2021-11-25 23:09:59 +0530

    RMX2020: Build libbluetooth_audio_session from source
    The older VNDK 30 blob when linked against the newer default bluetooth
    HW module, had a missing symbol:
    `_ZN7android9bluetooth5audio25BluetoothAudioSession_2_132invalidOffloadAudioConfigurationE`
    
    Since we build default bluetooth HW module from source, build
    libbluetooth_audio_session from source as well.
    
    Fixes: Bluetooth Audio on Android 12
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Change-Id: I13c09ecc37f709c575103d108acf5ca1b5aa2553

--- device.mk
+++ device.mk
@@ -108,6 +108,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libldacBT_dec \
     libldacBT_bco \
+    libbluetooth_audio_session.vendor \
     android.hardware.bluetooth.a2dp@1.0 \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.a2dp@1.0.vendor \
