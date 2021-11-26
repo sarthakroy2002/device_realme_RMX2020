a8de41f73e3c1ca79b8be8390817fcccbffdb8c0
TheMalachite <eliasgheeraert@gmail.com>
2021-11-26 21:40:26 +0530

    RMX2020: Build few Wifi blobs from source and add missing wifi blobs
    Change-Id: I778ccc23bc12a1d893a38641d3255a9240d8b2c5
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -496,26 +496,20 @@ PRODUCT_PACKAGES += \
     WifiOverlay
 
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0 \
+    android.hardware.wifi@1.0-service-lazy
+
+PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0.vendor \
-    android.hardware.wifi@1.1 \
     android.hardware.wifi@1.1.vendor \
-    android.hardware.wifi@1.2 \
     android.hardware.wifi@1.2.vendor \
-    android.hardware.wifi@1.3 \
     android.hardware.wifi@1.3.vendor \
-    android.hardware.wifi.supplicant@1.0 \
     android.hardware.wifi.supplicant@1.0.vendor \
-    android.hardware.wifi.supplicant@1.1 \
     android.hardware.wifi.supplicant@1.1.vendor \
-    android.hardware.wifi.supplicant@1.2 \
     android.hardware.wifi.supplicant@1.2.vendor \
-    android.hardware.wifi.supplicant@1.3 \
     android.hardware.wifi.supplicant@1.3.vendor \
-    android.hardware.wifi.hostapd@1.0 \
     android.hardware.wifi.hostapd@1.0.vendor \
-    android.hardware.wifi.hostapd@1.1 \
     android.hardware.wifi.hostapd@1.1.vendor \
+    android.hardware.wifi@1.3-impl \
     libkeystore-engine-wifi-hidl \
     libkeystore-wifi-hidl \
 
