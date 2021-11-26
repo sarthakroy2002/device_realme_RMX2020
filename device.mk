ca7308ca0fe6f4f01fbbbea4235bdd1c194ab11b
Sarthak Roy <sarthakroy2002@gmail.com>
2021-11-26 19:34:07 +0530

    RMX2020: Update DRM from Redmi Ares A12
    * from Redmi/ares/ares:12/SP1A.210812.016/21.11.8:user/release-keys
    
    Change-Id: I4b7dcd0e4e55cb545f472c51a89916b3d84fec19
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -149,7 +149,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3 \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4 \
+    android.hardware.drm@1.4.vendor \
+    android.hardware.drm@1.4-service.clearkey
 
 # fastbootd
 PRODUCT_PACKAGES += \
