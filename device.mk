340a0de69a93cde40aa89a64cbf9a4a5a3cca74c
Vaisakh Murali <mvaisakh@statixos.com>
2021-11-27 20:43:53 +0530

    RMX2020: Build Health hal from source
    Change-Id: I5121299b256d982abbc56ac2d28e10eb18d19fb1
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -162,10 +162,9 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0 \
+    android.hardware.health@2.1-service \
     android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.0-impl-2.1
 
 # HIDL
 PRODUCT_PACKAGES += \
