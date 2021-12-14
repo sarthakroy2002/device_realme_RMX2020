19b5efa25bbaa451a423d7e7380925756409dc23
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2021-12-14 19:42:39 +0530

    RMX2020: Enable Freeform Multiwindow
    Change-Id: I20473d0cdb9c4167beff3f80a0af158980ed43b0
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -152,6 +152,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.RMX2020
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
