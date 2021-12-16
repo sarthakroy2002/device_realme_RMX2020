1aa9e32a09a3c67c9cd06855b42493d40769894c
daniml3 <daniel@danielml.dev>
2021-12-16 20:23:35 +0530

    RMX2020: lights: Turn into an AIDL service
    * Based on AOSP's default lights AIDL implementation
    * Remove unused functions
    * Remove unused lights
    * Refactor specific lights handling
    
    Signed-off-by: daniml3 <daniel@danielml.dev>
    Change-Id: I92a55e9a0dd9a5dcebc7ec1653dc7aca6126b34d
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -204,7 +204,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.RMX2020
+    android.hardware.light-service.RMX2020
 
 # Media
 PRODUCT_COPY_FILES += \
