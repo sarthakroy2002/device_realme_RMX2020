40f2fd508df39bb1002c4370cc87e3c06f8a9ced
Sarthak Roy <sarthakroy2002@gmail.com>
2021-12-19 19:24:49 +0530

    RMX2020: Remove kernel modules init services
    * we are using inline kernel modules
    
    Change-Id: Ia7870d7c6a58a8a0b71c099d4217eb846bb8c566
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -367,15 +367,10 @@ PRODUCT_PACKAGES += \
     factory_init.project.rc \
     factory_init.rc \
     init.ago.rc \
-    init.bt_drv.rc \
     init.connectivity.rc \
-    init.fmradio_drv.rc \
-    init.gps_drv.rc \
     init.modem.rc \
     init.mt6768.rc \
     init.mt6768.usb.rc \
-    init.wmt_drv.rc \
-    init.wlan_drv.rc \
     init.project.rc \
     init.sensor_1_0.rc \
     meta_init.connectivity.rc \
