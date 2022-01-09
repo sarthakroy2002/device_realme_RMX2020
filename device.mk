636c261b52c8c63edc5bdc44351c79a0e9db31ad
Erfan Abdi <erfangplus@gmail.com>
2022-01-09 22:12:38 +0530

    RMX2020: Cleanup rootdir scripts
    Change-Id: I44e1b2f6d010f151545e3252a298618fa89d6e13

--- device.mk
+++ device.mk
@@ -376,24 +376,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.oppo.fingerprints.sh \
     init.oppo.wifi.debug.sh \
-    swap_enable.sh \
+    partition_permission.sh \
     perf_profile.sh \
-    factory_init.connectivity.rc \
-    factory_init.project.rc \
-    factory_init.rc \
-    init.ago.rc \
+    throttle.sh \
     init.connectivity.rc \
     init.modem.rc \
     init.mt6768.rc \
     init.mt6768.usb.rc \
-    init.project.rc \
     init.sensor_1_0.rc \
-    meta_init.connectivity.rc \
-    meta_init.modem.rc \
-    meta_init.project.rc \
-    meta_init.rc \
-    multi_init.rc \
-    fstab.enableswap \
     fstab.mt6768 \
     ueventd.mtk.rc
 
