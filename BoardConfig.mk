deb8d571dd45025546402d07c94debd8f1adf5ab
Sarthak Roy <sarthakroy2002@gmail.com>
2022-01-09 23:57:04 +0530

    RMX2020: Move props into configs
    Change-Id: I192698dd11cbcc42ce5f7d27ea67ab56d45b7ac0
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,9 +135,9 @@ BOARD_SUPER_PARTITION_ERROR_LIMIT := 6979321856
 BOARD_MAIN_PARTITION_LIST := product vendor system system_ext odm
 
 # Properties
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
-TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
+TARGET_ODM_PROP += $(DEVICE_PATH)/configs/props/odm.prop
 
 # Recovery
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
