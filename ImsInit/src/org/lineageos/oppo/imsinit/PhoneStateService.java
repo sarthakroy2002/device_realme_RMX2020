705c0d8f269e90456373aa1ce56502063bd0c2e9
Ayush <ayush@disroot.org>
2022-01-15 21:45:23 +0530

    RMX2020: ImsInit: Make IMS Work On Subsequent Reboots
    * After an reboot, IMS Service fails , to fix it, toggling Airplane mode was required, This fixed it Permanently
    
    Change-Id: Ibd798b3a3235bf7267adbf8349b2c2b52f772016

--- ImsInit/src/org/lineageos/oppo/imsinit/PhoneStateService.java
+++ ImsInit/src/org/lineageos/oppo/imsinit/PhoneStateService.java
@@ -9,7 +9,12 @@ import android.telephony.PhoneStateListener;
 import android.telephony.ServiceState;
 import android.telephony.TelephonyManager;
 import android.util.Log;
-
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkRequest;
+import android.os.Parcel;
+import android.os.ServiceManager;
 import com.android.ims.ImsManager;
 
 public class PhoneStateService extends Service {
@@ -17,27 +22,34 @@ public class PhoneStateService extends Service {
     private static ServiceState sLastState = null;
     
     private void handleServiceStateChanged(ServiceState serviceState) {
+        ConnectivityManager cm;
+        cm = (ConnectivityManager)this.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
         Log.i(LOG_TAG, "handleServiceStateChanged");
         if ((sLastState == null || sLastState.getDataRegState() != ServiceState.STATE_IN_SERVICE)
                 && serviceState.getDataRegState() == ServiceState.STATE_IN_SERVICE) {
             SharedPreferences prefs = getSharedPreferences("prefs", Context.MODE_PRIVATE);
         
-            if (prefs.getBoolean("first_time", true)) {
-                Log.i(LOG_TAG, "Skipping first boot");
-                prefs.edit().putBoolean("first_time", false).commit();
-                return;
-            }
-        
-            if (ImsManager.isEnhanced4gLteModeSettingEnabledByUser(this)) {
+	    // this looks stupid XD, will fix it later (atleast it works XD)
+            if (true) {
                 Log.i(LOG_TAG, "VoLTE enabled, trying to toggle it off and back on");
                 ImsManager.setEnhanced4gLteModeSetting(this, false);
                 new Thread(() -> {
                     try {
+                        // wait for 1 second
                         Thread.sleep(1000);
                     } catch (Exception e) {
                         // Ignore
                     }
+		            Log.i(LOG_TAG, "Trying to request network!");
                     ImsManager.setEnhanced4gLteModeSetting(this, true);
+		            NetworkRequest.Builder req = new NetworkRequest.Builder();
+                    req.addCapability(NetworkCapabilities.NET_CAPABILITY_IMS);
+                    cm.requestNetwork(req.build(), new ConnectivityManager.NetworkCallback() {
+                    @Override
+                           public void onAvailable(Network network) {
+                            Log.i(LOG_TAG, "Network "+ network  +" is available!");
+                         }
+                    });
                 }).start();
             }
         }
