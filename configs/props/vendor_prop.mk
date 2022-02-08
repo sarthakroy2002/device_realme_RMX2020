0cdc6f70d2dd67b4608ebe5540254fd2658004e7
SamarV-121 <samarvispute121@pm.me>
2022-02-08 18:14:54 +0530

    RMX2020: props: Remove obsolete debug.sf.disable_backpressure property
     * It has been removed in Android S
     * LineageOS/android_frameworks_native@2ec5391
    
    Change-Id: I23a210a37ad257f0c8b7a3c5e1d59b2ac20cde9a
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- configs/props/vendor_prop.mk
+++ configs/props/vendor_prop.mk
@@ -6,7 +6,6 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.mtk-stack-trace-file=/data/anr/mtk_traces.txt \
-    debug.sf.disable_backpressure=1 \
     debug.sf.latch_unsignaled=1 \
     drm.service.enabled=true \
     persist.adb.nonblocking_ffs=0 \
