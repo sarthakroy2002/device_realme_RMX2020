f2b62c8886742244abf89925d00c85c7fdf42528
SamarV-121 <samarvispute121@gmail.com>
2022-02-19 11:47:23 +0530

    RMX2020: Compile ramdisk fstab as a package
    Change-Id: Ia239013db1bda2d4029390f4166a28d3c3d175b5

--- device.mk
+++ device.mk
@@ -145,9 +145,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/rootdir/etc/fstab.mt6768:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6768
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.RMX2020
@@ -357,6 +354,7 @@ PRODUCT_PACKAGES += \
     init.project.rc \
     init.sensor_1_0.rc \
     fstab.mt6768 \
+    fstab.ramdisk \
     ueventd.mtk.rc
 
 # Recovery
