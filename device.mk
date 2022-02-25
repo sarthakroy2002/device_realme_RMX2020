4831e14a0f96d18d9f4c4291ab12f108b924c2cc
Sarthak Roy <sarthakroy2002@gmail.com>
2022-02-25 10:16:18 +0530

    RMX2020: Add back prebuilt libstdc++
    - Doesnt build from source
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I4c8fde6e9acb9ba33a99488258b1066482e85489

--- device.mk
+++ device.mk
@@ -111,9 +111,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.5.vendor \
     android.hardware.camera.provider@2.4.vendor
 
-PRODUCT_PACKAGES += \
-    libstdc++.vendor
-
 # Disable Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
