bf041868d76cdc8e14e1547d235c5144b878f42c
Pranav Vashi <neobuddy89@gmail.com>
2022-03-26 19:43:24 +0530

    RMX2020: releasetools: Use appropriate message on flashing vbmeta and dtbo
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I1152abf3ede89c86b2039426e42c2ba5cc1a2e8d

--- releasetools.py
+++ releasetools.py
@@ -34,7 +34,7 @@ def AddImage(info, basename, dest):
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
 def OTA_InstallEnd(info):
-  info.script.Print("Patching firmware images...")
+  info.script.Print("Patching dtbo and vbmeta images...")
   AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
   return
