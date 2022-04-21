b45da1deca14fdead615c19c7ae2565384834431
Sarthak Roy <sarthakroy2002@gmail.com>
2022-04-21 20:06:34 +0530

    RMX2020: Dont copy com.nxp.mifare permission
    - We dont need it.
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Ic9b0de75ba7c8765d282785c0aef8572446321cc

--- device.mk
+++ device.mk
@@ -291,7 +291,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/android.hardware.nfc.hcef.xml \
     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/android.hardware.nfc.uicc.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
 # Pixel Charger
