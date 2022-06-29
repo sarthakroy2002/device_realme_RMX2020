b6a56f59bc4bee43602bbe2a95693ddfdfd53889
Sarthak Roy <sarthakroy2002@gmail.com>
2022-06-29 09:54:19 +0530

    RMX2020: Remove Google Dialer Call recording
    - Doesn't work
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I132eed1fd961dcbbb7bf5fa28813cfc22510a8b1

--- device.mk
+++ device.mk
@@ -146,10 +146,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor \
     android.hardware.gatekeeper@1.0-impl
 
-# Google Dialer Call recording
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
-
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.0.vendor \
