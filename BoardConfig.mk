b453717a96f253c8a6265a7eb20b655148727560
Sal Savage <salsavage@google.com>
2022-08-18 18:22:35 +0530

    RMX2020: Set default the Bluetooth class of device
    The class of device is a string with a list of uint8t values:
    90,2,12
    
    The meaning is as follows:
    {Service Field, Major class, Minor class}
    
    Service Field: 0x5A -> 90
        Bit 17: Networking
        Bit 19: Capturing
        Bit 20: Object Transfer
        Bit 22: Telephony
    MAJOR_CLASS: 0x02 -> 2 (Phone)
    MINOR_CLASS: 0x0C -> 12 (Smart Phone)
    
    Bug: 217452259
    Test: make -j; -- check that bluetooth.device.class_of_device is
    correct
    
    Change-Id: I07e83ca352ff969be0e53a0f63b6c940b6c34dfb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -131,6 +131,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/props/system_ext.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
 
