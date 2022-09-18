bc71c3ed7cec3a2ecddbc6309efe92fdc0d99a03
Albert I <kras@raphielgang.org>
2022-09-18 12:11:23 +0530

    RMX2020: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc

--- device.mk
+++ device.mk
@@ -161,6 +161,10 @@ PRODUCT_PACKAGES += \
     libhwbinder.vendor \
     libhidltransport.vendor
 
+# Inherit several Android Go Configurations (Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/idc/mtk-kpd.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/mtk-kpd.idc \
