63d55433c97342de04f035766d4bc7c40c0ff1ba
bengris32 <bengris32@protonmail.ch>
2022-10-07 22:18:47 +0530

    RMX2020: Link audio modules against prebuilt libalsautils.so
    * Fixes USB DAC.
    
    Change-Id: I39893c06fc4d1fa8ccc203628abd167ae58c9b8d
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- extract-files.sh
+++ extract-files.sh
@@ -58,8 +58,12 @@ function blob_fixup {
         lib/libsink.so)
             "${PATCHELF}" --add-needed "libshim_vtservice.so" "${2}"
             ;;
-        vendor/lib/hw/audio.primary.mt6768.so)
+        vendor/lib*/hw/audio.primary.mt6768.so)
             "${PATCHELF}" --replace-needed "libmedia_helper.so" "libmedia_helper-v30.so" "${2}"
+            "${PATCHELF}" --replace-needed "libalsautils.so" "libalsautils-v30.so" "${2}"
+            ;;
+        vendor/lib*/hw/audio.usb.mt6768.so)
+            "${PATCHELF}" --replace-needed "libalsautils.so" "libalsautils-v30.so" "${2}"
             ;; 
         vendor/bin/hw/android.hardware.wifi@1.0-service-lazy-mediatek)
             "${PATCHELF}" --replace-needed "libwifi-hal.so" "libwifi-hal-mtk.so" "${2}"
