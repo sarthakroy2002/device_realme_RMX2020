5ff9bf930808295a5f144225a5b883c78b5a36df
Sarthak Roy <sarthakroy2002@gmail.com>
2022-10-12 19:41:36 +0530

    RMX2020: Switch Back to Stock A10 Thermal Stack
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I653314c121f67069190283efeb020de862bfdfca

--- device.mk
+++ device.mk
@@ -381,9 +381,8 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0.vendor \
-    android.hardware.thermal@2.0.vendor \
-    android.hardware.thermal@1.0-impl
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
 
 # TinyXML
 PRODUCT_PACKAGES += \
