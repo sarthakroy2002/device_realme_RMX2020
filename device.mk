e30af8b63ea9451ea9416d69b40595e1202a28ac
Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
2022-10-12 23:06:31 +0530

    RMX2020: Remove deprecated android.hardware.thermal@1.0 HAL
    Remove deprecated android.hardware.thermal@1.0 HAL from
    device manifest.
    
    Bug: 154012150
    Change-Id: I884e6b96cac4ebc3abcfaf309388d526a30dc5bd

--- device.mk
+++ device.mk
@@ -379,11 +379,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtextclassifier_hash.vendor
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
-
 # TinyXML
 PRODUCT_PACKAGES += \
     libtinyxml
