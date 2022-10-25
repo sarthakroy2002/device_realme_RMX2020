5df3d68f8f7c190c158db38f69c0b679abab716e
Akash Srivastava <akashniki@gmail.com>
2022-10-25 21:46:58 +0530

    RMX2020: Build android.hidl.memory.block and android.hidl.memory@1.0-impl
    * Used for dynamic memory management
    
    Change-Id: I7f39d3c346498a527294c284e8577662ac844f9e

--- device.mk
+++ device.mk
@@ -161,6 +161,10 @@ PRODUCT_PACKAGES += \
     libhwbinder.vendor \
     libhidltransport.vendor
 
+PRODUCT_PACKAGES += \
+    android.hidl.memory.block@1.0 \
+    android.hidl.memory@1.0-impl
+
 # Inherit several Android Go Configurations (Beneficial for everyone, even on non-Go devices)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
