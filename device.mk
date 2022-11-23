5a3f21967bb463a649a0f6f1fbcf794a964c4b8f
Omkar Chandorkar <gotenksIN@aospa.co>
2022-11-23 20:17:03 +0530

    RMX2020: overlay: Configure aux camera for Aperture
    Change-Id: I14e9bf9ba71c9d9f4aca04f3b558a1d72e248a60
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- device.mk
+++ device.mk
@@ -338,6 +338,7 @@ PRODUCT_PACKAGES += \
 
 # Runtime Resource Overlays 
 PRODUCT_PACKAGES += \
+    ApertureOverlayRMX2020 \
     CarrierConfigOverlayRMX2020 \
     DialerOverlayRMX2020 \
     DocumentsUIOverlayRMX2020 \
