521f3c5f36d3563038391071f844045446f8cc2b
Bruno Martins <bgcngm@gmail.com>
2022-12-06 17:59:00 +0000

    RMX2020: doze: Fix compilation for QPR1 merge
    Change-Id: Ifa602ed1ae0734dbda40663770f2e54075bfa208

--- doze/src/org/lineageos/settings/doze/DozeSettingsActivity.kt
+++ doze/src/org/lineageos/settings/doze/DozeSettingsActivity.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 The LineageOS Project
+ * Copyright (C) 2021-2022 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -7,7 +7,7 @@ package org.lineageos.settings.doze
 
 import android.os.Bundle
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity
-import com.android.settingslib.collapsingtoolbar.R
+import com.android.settingslib.widget.R
 
 class DozeSettingsActivity : CollapsingToolbarBaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
