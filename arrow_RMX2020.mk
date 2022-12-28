409fc56c3016955c1158d15c68f41aedb602170e
Tim Zimmermann <tim@linux4.de>
2022-12-28 13:59:58 +0530

    RMX2020: Inherit non_ab_device.mk
    * This is required to build applypatch for updating recovery
    
    Change-Id: I38053691f7724b5d6c30159c410d5d04ac136180

--- arrow_RMX2020.mk
+++ arrow_RMX2020.mk
@@ -7,6 +7,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2020/device.mk)
