452e68b02558259500390ab7303ad300a78aff59
Lunarixus <Nathanb644@hotmail.co.uk>
2022-12-30 23:27:24 +0530

    RMX2020: Remove Camera packages
    Co-authored-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I5d965b38b12cd82cd845722f5c92a70f62c740d7

---
+++ app/RemoveCameraPackages/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemoveCameraPackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := Snap Camera2 GCamGOPrebuilt-V3 GCamGOPrebuilt-V2 GCamGOPrebuilt GrapheneCamera
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
