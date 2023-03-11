c344981f8ad8e6565fba52d915a9958d7f491c06
Sarthak Roy <sarthak@pixelos.net>
2023-03-11 15:15:35 +0530

    RMX2020: Refactor and Fixup RealmeParts
    Co-authored-by: SamarV-121 <samarvispute121@gmail.com>
    Co-authored-by: P-Salik <peersalik16@gmail.com>
    Change-Id: I060e9e0517f6a9bf8ee8ac710721a1869a4d92ed
    Signed-off-by: Sarthak Roy <sarthak@pixelos.net>

--- app/RealmeParts/parts.mk
+++ app/RealmeParts/parts.mk
@@ -9,5 +9,4 @@ PRODUCT_PACKAGES += \
     RealmeParts
 
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/app/RealmeParts/init/perf_profile.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/perf_profile.sh \
-    $(DEVICE_PATH)/app/RealmeParts/init/cabc.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/cabc.rc
+    $(DEVICE_PATH)/app/RealmeParts/init/perf_profile.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/perf_profile.sh
