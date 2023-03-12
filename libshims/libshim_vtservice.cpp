ad1ce73775a877476fab93b750764372ac0bf1e8
Arian <arian.kulmer@web.de>
2023-03-12 23:29:34 +0530

    RMX2020: Provide a proper shim for libsink
    added opPackageName as a parameter to the constructor
    of AudioTrack which breaks our libsink
    which was compiled for an older libaudioclient.
    
    * fixes the following error:
    CANNOT LINK EXECUTABLE "/system/bin/vtservice": cannot locate symbol "_ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_output_flags_tPFviPvS4_ES4_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tjiPK18audio_attributes_tbfi" referenced by "/system/lib/libsink.so"
    
    https: //github.com/LineageOS/android_frameworks_av/commit/fd90fdfe2a611ae824a32e236da288b1fdfd445d
    Change-Id: Ice2cf524634df10f79261a09d40374fcc2a74abb

--- libshims/libshim_vtservice.cpp
+++ libshims/libshim_vtservice.cpp
@@ -1,5 +1,32 @@
+#include <media/AudioTrack.h>
+
+using namespace android;
+
 extern "C" {
-   void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_output_flags_tPFviPvS4_ES4_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tjiPK18audio_attributes_tbfi() {}
-   void _ZN7android11AudioSourceC1E14audio_source_tRKNS_8String16Ejjjjii28audio_microphone_direction_tf() {}
-   void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb() {}
+// Old constructor without opPackageName
+void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_output_flags_tPFviPvS4_ES4_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tjiPK18audio_attributes_tbfi(
+        audio_stream_type_t streamType,
+        uint32_t sampleRate,
+        audio_format_t format,
+        audio_channel_mask_t channelMask,
+        size_t frameCount,
+        audio_output_flags_t flags,
+        android::AudioTrack::callback_t cbf,
+        void* user,
+        int32_t notificationFrames,
+        audio_session_t sessionId,
+        android::AudioTrack::transfer_type transferType,
+        const audio_offload_info_t *offloadInfo,
+        uid_t uid,
+        pid_t pid,
+        const audio_attributes_t* pAttributes,
+        bool doNotReconnect,
+        float maxRequiredSpeed,
+        audio_port_handle_t selectedDeviceId) {
+    const std::string& opPackageName = "com.mediatek.ims";
+    new android::AudioTrack(streamType, sampleRate, format, channelMask, frameCount, flags, cbf,
+                            user, notificationFrames, sessionId, transferType, offloadInfo, uid,
+                            pid, pAttributes, doNotReconnect, maxRequiredSpeed, selectedDeviceId,
+                            opPackageName);
+        }
 }
