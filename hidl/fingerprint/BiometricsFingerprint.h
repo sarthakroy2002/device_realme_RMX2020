be009d60f49d8c6de2f5cd271207039e09329b5f
Bruno Martins <bgcngm@gmail.com>
2023-06-01 12:23:53 +0530

    RMX2020: fingerprint: Use pragma once
    Change-Id: I86380589d87d5a487495948bf08968e0945865b8

--- hidl/fingerprint/BiometricsFingerprint.h
+++ hidl/fingerprint/BiometricsFingerprint.h
@@ -14,8 +14,7 @@
  * limitations under the License.
  */
 
-#ifndef ANDROID_HARDWARE_BIOMETRICS_FINGERPRINT_V2_1_BIOMETRICSFINGERPRINT_H
-#define ANDROID_HARDWARE_BIOMETRICS_FINGERPRINT_V2_1_BIOMETRICSFINGERPRINT_H
+#pragma once
 
 #include <log/log.h>
 #include <android/log.h>
@@ -84,5 +83,3 @@ private:
 }  // namespace biometrics
 }  // namespace hardware
 }  // namespace android
-
-#endif  // ANDROID_HARDWARE_BIOMETRICS_FINGERPRINT_V2_1_BIOMETRICSFINGERPRINT_H
