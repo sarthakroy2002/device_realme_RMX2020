13fcf8744f0e2e88cba577d2a9b59dc3012c28b2
Maanush Putcha <p.maanush@gmail.com>
2023-06-12 11:30:38 +0530

    RMX2020: Shim another libshowlogo symbol
    CANNOT LINK EXECUTABLE kpoc_charger: cannot locate symbol _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataE referenced by /system/lib/libshowlogo.so
    
    * Q : https://github.com/LineageOS/android_frameworks_native/blob/lineage-17.1/libs/gui/SurfaceComposerClient.cpp#L1316
    
    * R : https://github.com/LineageOS/android_frameworks_native/blob/lineage-18.1/libs/gui/SurfaceComposerClient.cpp#L1600
    
    Change-Id: Idf144240c281d1af24d4e65c71c6cc9b1e3bda89
    Signed-off-by: Maanush Putcha <p.maanush@gmail.com>

--- libshims/libshim_showlogo.cpp
+++ libshims/libshim_showlogo.cpp
@@ -2,7 +2,14 @@
 
 namespace android {
 
-extern "C" void _ZN7android21SurfaceComposerClient11Transaction20setDisplayProjectionERKNS_2spINS_7IBinderEEEjRKNS_4RectES9_(const sp<IBinder>& token,uint32_t orientation,const android::Rect& layerStackRect,const android::Rect& displayRect) {
-  t->setDisplayProjection(token, static_cast<android::ui::Rotation>(orientation), layerStackRect, displayRect);
+  extern "C" void _ZN7android21SurfaceComposerClient11Transaction20setDisplayProjectionERKNS_2spINS_7IBinderEEEjRKNS_4RectES9_(const sp<IBinder>& token,uint32_t orientation,const android::Rect& layerStackRect,const android::Rect& displayRect) {
+    t->setDisplayProjection(token, static_cast<android::ui::Rotation>(orientation), layerStackRect, displayRect);
+  }
+
+  extern "C" void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataEPj(const String8& name, uint32_t w, uint32_t h, PixelFormat format, uint32_t flags, SurfaceControl* parent, LayerMetadata metadata, uint32_t* outTransformHint);
+
+  extern "C" void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataE(const String8& name, uint32_t w, uint32_t h, PixelFormat format, uint32_t flags, SurfaceControl* parent, LayerMetadata metadata) {
+	  _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataEPj(name, w, h, format, flags, nullptr, metadata, nullptr);
+	}
 
 }
