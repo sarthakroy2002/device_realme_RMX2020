b4fcd3fb4500f40bb1bba929dd79d08cf6630d5b
SamarV-121 <samarvispute121@gmail.com>
2023-06-12 11:35:13 +0530

    RMX2020: libshims: Update showlogo shim to S
    Change-Id: I7b2a3de988d840df6543b4148f247c3141170a76

--- libshims/libshim_showlogo.cpp
+++ libshims/libshim_showlogo.cpp
@@ -1,18 +1,24 @@
 #include <gui/SurfaceComposerClient.h>
 
-namespace android {
+using namespace android;
 
-  extern "C" void _ZN7android21SurfaceComposerClient11Transaction20setDisplayProjectionERKNS_2spINS_7IBinderEEEjRKNS_4RectES9_(const sp<IBinder>& token,uint32_t orientation,const android::Rect& layerStackRect,const android::Rect& displayRect) {
-    android::SurfaceComposerClient::Transaction *t = nullptr;
-    t->setDisplayProjection(token, static_cast<android::ui::Rotation>(orientation), layerStackRect, displayRect);
+extern "C" {
+
+  void _ZN7android21SurfaceComposerClient11Transaction20setDisplayProjectionERKNS_2spINS_7IBinderEEEjRKNS_4RectES9_(const sp<IBinder>& token,uint32_t orientation,const android::Rect& layerStackRect,const android::Rect& displayRect) {
+    SurfaceComposerClient::Transaction *t = nullptr;
+    t->setDisplayProjection(token, static_cast<ui::Rotation>(orientation), layerStackRect, displayRect);
   }
 
-  extern "C" void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataEPj(const String8& name, uint32_t w, uint32_t h, PixelFormat format, uint32_t flags, SurfaceControl* parent, LayerMetadata metadata, uint32_t* outTransformHint);
+  void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijRKNS_2spINS_7IBinderEEENS_13LayerMetadataEPj(const String8& name, uint32_t w, uint32_t h, PixelFormat format, uint32_t flags, const sp<IBinder>& parentHandle, LayerMetadata metadata, uint32_t* outTransformHint);
+  
+  void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataE(const String8& name, uint32_t w, uint32_t h, PixelFormat format, uint32_t flags, SurfaceControl* parent, LayerMetadata metadata) {
+    _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijRKNS_2spINS_7IBinderEEENS_13LayerMetadataEPj(name, w, h, format, flags, nullptr, metadata, nullptr);
+  }
 
-  extern "C" void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataE(const String8& name, uint32_t w, uint32_t h, PixelFormat format, uint32_t flags, SurfaceControl* parent, LayerMetadata metadata) {
-	  _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataEPj(name, w, h, format, flags, nullptr, metadata, nullptr);
-	}
+  void _ZN7android14SurfaceControlD0Ev(void);
 
-  extern "C" void _ZN7android14SurfaceControl7releaseEv() { }
+  void _ZN7android14SurfaceControl7releaseEv(void) {
+    _ZN7android14SurfaceControlD0Ev();
+  }
 
 }
