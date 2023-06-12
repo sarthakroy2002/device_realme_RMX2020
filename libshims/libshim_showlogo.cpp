f16cfa2a9c5f2c74ae0c184d0dfe2a1297321759
Maanush Putcha <p.maanush@gmail.com>
2023-06-12 11:33:04 +0530

    RMX2020: libshims: Define t
    * Needed for t->setDisplayProjection(token, static_cast<android::ui::Rotation>(orientation), layerStackRect, displayRect);
    
    * Taken from AICP/device_htc_hima-common@4e806f1#diff-efb416ffe9ab0710bd46ee126e7b4e8b5fb3a32f8e3d7e2d3a8871b900ec36a9R34
    
    Change-Id: I8909686c210a07907b57f14e821298cb6fac6fd2
    Signed-off-by: Maanush Putcha <p.maanush@gmail.com>

--- libshims/libshim_showlogo.cpp
+++ libshims/libshim_showlogo.cpp
@@ -3,6 +3,7 @@
 namespace android {
 
   extern "C" void _ZN7android21SurfaceComposerClient11Transaction20setDisplayProjectionERKNS_2spINS_7IBinderEEEjRKNS_4RectES9_(const sp<IBinder>& token,uint32_t orientation,const android::Rect& layerStackRect,const android::Rect& displayRect) {
+    android::SurfaceComposerClient::Transaction *t = nullptr;
     t->setDisplayProjection(token, static_cast<android::ui::Rotation>(orientation), layerStackRect, displayRect);
   }
 
