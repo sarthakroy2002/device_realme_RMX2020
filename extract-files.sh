9fa605d3c8cda8eda2c88c2699e183256d40e04b
R0rt1z2 <rogerortizleal@gmail.com>
2023-09-03 22:07:35 +0530

    RMX2020: Hexpatch camera lib to increase maximum JPEG streams for an app
    * Increase maxAppJpegStreamNum from 5 to 11 in order to fix GCam lags.
    
    Change-Id: I67fcfedaaa747e3f444bf3f9a6135516e6570bf4

--- extract-files.sh
+++ extract-files.sh
@@ -86,6 +86,10 @@ function blob_fixup() {
         vendor/lib64/hw/android.hardware.thermal@2.0-impl.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "$2"
             ;;
+        vendor/lib64/libmtkcam_featurepolicy.so)
+            # evaluateCaptureConfiguration()
+            sed -i "s/\x34\xE8\x87\x40\xB9/\x34\x28\x02\x80\x52/" "$2"
+            ;;
     esac
 }
 
