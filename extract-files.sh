7d02173b3c6998a195bc15cd6128832996d60d41
SamarV-121 <samarvispute121@pm.me>
2023-09-03 18:59:47 +0530

    RMX2020: Link older libutils for PQ and dfps HALs
    * Abort message: 'incStrongRequireStrong() called on 0xb400007c5afa7250 which isn't already owned'
    
    Change-Id: I8b014200f5d4ed8fe160f2176218785443b6e9a3

--- extract-files.sh
+++ extract-files.sh
@@ -67,6 +67,12 @@ function blob_fixup() {
         vendor/etc/init/android.hardware.bluetooth@1.0-service-mediatek.rc)
             sed -i '/vts/Q' "$2"
             ;;
+        vendor/lib64/hw/dfps.mt6768.so)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
+            ;;
+        vendor/lib64/hw/vendor.mediatek.hardware.pq@2.6-impl.so)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
+            ;;
     esac
 }
 
