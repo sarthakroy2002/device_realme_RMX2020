84ae5e6c900638beb72a7405a08ff3f2b5d4daa8
Sarthak Roy <sarthakroy2002@gmail.com>
2023-09-08 15:27:08 +0530

    Revert "RMX2020: Include firmware in build"
    This reverts commit e8f32b4bf15978c4bd8c36e54aa5584a1927466c.
    
    Change-Id: Ib4fa01147b6498c8b47788c018cf78beffecf68e

--- releasetools.py
+++ releasetools.py
@@ -15,64 +15,24 @@
 # limitations under the License.
 
 import common
+import re
 
 def FullOTA_InstallEnd(info):
-  OTA_InstallEnd(info, False)
+  OTA_InstallEnd(info)
+  return
 
 def IncrementalOTA_InstallEnd(info):
-  OTA_InstallEnd(info, True)
+  OTA_InstallEnd(info)
+  return
 
-def AddImageOnly(info, basename, incremental, firmware):
-  if incremental:
-    input_zip = info.source_zip
-  else:
-    input_zip = info.input_zip
-  if firmware:
-    data = input_zip.read("RADIO/" + basename)
-  else:
-    data = input_zip.read("IMAGES/" + basename)
-  common.ZipWriteStr(info.output_zip, basename, data)
+def AddImage(info, basename, dest):
+  name = basename
+  data = info.input_zip.read("IMAGES/" + basename)
+  common.ZipWriteStr(info.output_zip, name, data)
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
-def AddImage(info, basename, dest, incremental):
-  AddImageOnly(info, basename, incremental, False)
-  info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
-  info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
-
-def OTA_InstallEnd(info, incremental):
-  AddImage(info, "vbmeta.img", "/dev/block/platform/bootdevice/by-name/vbmeta", incremental)
-
-  bin_map = {
-      'logo': ['logo']
-      }
-
-  img_map = {
-      'dtbo': ['dtbo'],
-      'gz': ['gz1', 'gz2'],
-      'lk': ['lk', 'lk2'],
-      'md1img': ['md1img'],
-      'scp': ['scp1', 'scp2'],
-      'spmfw': ['spmfw'],
-      'sspm': ['sspm_1', 'sspm_2'],
-      'tee': ['tee1', 'tee2']
-      }
-
-  pl = 'preloader_emmc'
-  pl_part = ['mmcblk0boot0', 'mmcblk0boot1']
-
-  fw_cmd = 'ui_print("Patching radio images unconditionally...");\n'
-
-  AddImageOnly(info, "{}.img".format(pl), incremental, True)
-  for part in pl_part:
-      fw_cmd += 'package_extract_file("{}.img", "/dev/block/{}");\n'.format(pl, part)
-
-  for img in img_map.keys():
-    AddImageOnly(info, '{}.img'.format(img), incremental, True)
-    for part in img_map[img]:
-      fw_cmd += 'package_extract_file("{}.img", "/dev/block/platform/bootdevice/by-name/{}");\n'.format(img, part)
-
-  for _bin in bin_map.keys():
-    AddImageOnly(info, '{}.bin'.format(_bin), incremental, True)
-    for part in bin_map[_bin]:
-      fw_cmd += 'package_extract_file("{}.bin", "/dev/block/platform/bootdevice/by-name/{}");\n'.format(_bin, part)
-
-  info.script.AppendExtra(fw_cmd)
+def OTA_InstallEnd(info):
+  info.script.Print("Patching firmware images...")
+  AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
+  AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
+  return
