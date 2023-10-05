6c587c4e919a4f9cb98d630f3670e8e088cee4d1
Sarthak Roy <sarthakroy2002@gmail.com>
2023-10-05 13:18:47 +0530

    RMX2020: Build `android.frameworks.sensorservice@1.0.vendor`
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Iff9eaf0b79476e2b86aa21154f8b4030bbe1d16b

--- device.mk
+++ device.mk
@@ -359,7 +359,8 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
-    android.hardware.sensors@2.0.vendor \
+    android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.sensors@2.0.vendor
 
 PRODUCT_PACKAGES += \
     libsensorndkbridge
