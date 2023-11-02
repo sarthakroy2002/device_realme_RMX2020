4f923892b88168e911a0353079f9c58acb64e8f6
Debayan Kar <kardebayan3@gmail.com>
2023-11-02 11:49:43 +0530

    RMX2020: Switch to AOSP sensor multihal service
    - Also use subhal reference to [1].
    
    [1]: https://github.com/Nothing-2A/android_device_nothing_Aerodactyl/commit/cf4634d39a9a0fd0e547012abe533eff6dbceb5a
    
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>
    Change-Id: I245011e26fc2a298baff96baab05a7b1686749f5

--- device.mk
+++ device.mk
@@ -314,7 +314,8 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.frameworks.sensorservice@1.0
+    android.hardware.sensors-service.multihal \
+    android.hardware.sensors@2.0-subhal-impl-1.0
 
 PRODUCT_PACKAGES += \
     libsensorndkbridge
