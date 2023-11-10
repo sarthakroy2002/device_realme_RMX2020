3d1cce3d49b8cb359e1d57690433039ce8b1b132
Sarthak Roy <sarthakroy2002@gmail.com>
2023-11-10 11:24:56 +0530

    RMX2020: Switch to prebuilt USB HAL
    * USB Tethering doesn't work with basic USB HAL
    * Reverts https://github.com/sarthakroy2002/device_realme_RMX2020/commit/b115a6074f030ab279422eca26e8fc0493053fc2
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I52d6eacbf3c188ba96a7b2b5a8c9491cc4d6d2c9

--- device.mk
+++ device.mk
@@ -378,7 +378,8 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.RMX2020
+    android.hardware.usb@1.1.vendor:64 \
+    android.hardware.usb.gadget@1.1.vendor:64
 
 # Vibrator
 TARGET_VIBRATOR_SUPPORTS_EFFECTS := true
