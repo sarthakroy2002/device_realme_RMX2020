000e2871070883ad2c107b1609a1c983b904db54
SamarV-121 <samar@samarv121.dev>
2024-10-14 20:46:04 +0530

    RMX2020: Auto generate symlinks while extraction
     - depends on https://review.lineageos.org/c/LineageOS/android_tools_extract-utils/+/386540
    
    Change-Id: I6846c11c7ed2fa2eb7d237321f7d30677c311d05

--- Android.mk
+++ Android.mk
@@ -10,36 +10,4 @@ ifeq ($(TARGET_DEVICE),RMX2020)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
-GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
-GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
-$(GATEKEEPER_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	$(hide) ln -sf libSoftGatekeeper.so $@
-
-GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.mt6768.so
-GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.oppo6769.so
-GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.mt6768.so
-GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.oppo6769.so
-$(GATEKEEPER_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	$(hide) ln -sf libMcGatekeeper.so $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(GATEKEEPER_SYMLINK)
-
-VULKAN_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/vulkan.mt6768.so
-VULKAN_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.mt6768.so
-$(VULKAN_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	$(hide) ln -sf ../egl/libGLES_mali.so $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(VULKAN_SYMLINK)
-
-KMSETKEY_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/kmsetkey.default.so
-KMSETKEY_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/kmsetkey.default.so
-$(KMSETKEY_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	$(hide) ln -sf kmsetkey.trustonic.so $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(KMSETKEY_SYMLINK)
-
 endif
