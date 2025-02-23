f095905d2deb09c2fb420d48352c9d17c268ebf5
Sarthak Roy <sarthakroy2002@gmail.com>
2025-02-23 19:04:01 +0530

    RMX2020: Build FMRadio
    Change-Id: I4e0ebd7352ad04e97ce65d3aec5a1b9d030fb3c3
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -109,6 +109,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.RMX2020
 
+# FM Radio
+PRODUCT_PACKAGES += \
+    FMRadio
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
