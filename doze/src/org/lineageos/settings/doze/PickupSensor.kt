66b8154bfd13076714b76a19512b0f800c7ae035
Edwin Moquete <edwinmmoquete@gmail.com>
2025-03-11 16:27:00 +0000

    RMX2020: doze: Fix compilation on V QPR2
    Change-Id: If1693603abc530af3b761b53396f2dda8225177e

--- doze/src/org/lineageos/settings/doze/PickupSensor.kt
+++ doze/src/org/lineageos/settings/doze/PickupSensor.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021-2024 The LineageOS Project
+ * Copyright (C) 2021-2025 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -13,6 +13,7 @@ import android.hardware.SensorManager
 import android.os.PowerManager
 import android.os.SystemClock
 import android.util.Log
+import android.view.Display
 
 import java.util.concurrent.Executors
 
@@ -39,7 +40,10 @@ class PickupSensor(
             if (Utils.isPickUpSetToWake(context)) {
                 wakeLock.acquire(WAKELOCK_TIMEOUT_MS)
                 powerManager.wakeUp(
-                    SystemClock.uptimeMillis(), PowerManager.WAKE_REASON_GESTURE, TAG
+                    SystemClock.uptimeMillis(),
+                    PowerManager.WAKE_REASON_GESTURE,
+                    TAG,
+                    Display.DEFAULT_DISPLAY
                 )
             } else {
                 Utils.launchDozePulse(context)
