4c1975fe83b65420c1f6498fc3070504dc9db768
Nolen Johnson <johnsonnolen@gmail.com>
2025-03-16 10:09:21 +0530

    RMX2020: Drop old Android Go configurations
    * These break build on QPR2.
    
    Change-Id: Ib2a80892f18d117e27dd703fa74122b24d900dcf

--- device.mk
+++ device.mk
@@ -138,10 +138,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndservicemanager
 
-# Inherit several Android Go Configurations (Beneficial for everyone, even on non-Go devices)
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
-
 # Kernel
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
