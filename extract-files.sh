aabbbad58dcb461a255cc0f9f000b7128df01d4c
Sarthak Roy <sarthakroy2002@gmail.com>
2025-03-16 15:41:43 +0530

    RMX2020: Patch 32bit lib3a.flash.so too
    * extends https://github.com/PixelOS-Devices/device_realme_RMX2020/commit/f216e8459829389f69ec02fd94e12a9f74909bf2
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: If9d5e9f3b7542cc41851f61abc6089189d21399b

--- extract-files.sh
+++ extract-files.sh
@@ -109,7 +109,7 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q libui_shim.so "$2" || "$PATCHELF" --add-needed libui_shim.so "$2"
             ;;
-        vendor/lib64/lib3a.flash.so)
+        vendor/lib*/lib3a.flash.so)
             [ "$2" = "" ] && return 0
             grep -q "liblog.so" "${2}" || "${PATCHELF_0_17_2}" --add-needed "liblog.so" "${2}"
             ;;
