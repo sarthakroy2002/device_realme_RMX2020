3520862dba23d9e6c6801dd81006ae1f14c86f4f
TheMalachite <eliasgheeraert@gmail.com>
2025-04-16 02:32:06 +0530

    RMX2020: Use device tree path directly
    Relying over Android.mk just for this isn't worth it.
    
    Change-Id: I107cf00853edbc00f03b2a6ee2dd04db24440458

--- device.mk
+++ device.mk
@@ -330,7 +330,7 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal-service.mediatek
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+    $(DEVICE_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
 # Update
 AB_OTA_UPDATER := false
