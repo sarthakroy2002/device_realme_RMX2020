aeb5854e46e1083dddf80c2d13e439aa5319e8b4
Mashopy <eliasgheeraert@gmail.com>
2025-06-18 17:53:14 +0530

    RMX2020: Patch mtkfusionrild to load libutils-v32
    * Fixes RIL in Android 16
    
    Change-Id: Ic9d3ecad755f5a7fcda7deccfbfbb31a305752c1
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -129,6 +129,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libsink.so" "libsink-mtk.so" "${2}"
             ;;
+        vendor/bin/hw/mtkfusionrild)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "libutils-v32.so" "${2}"
+            ;;
         *)
             return 1
             ;;
