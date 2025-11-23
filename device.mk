11657ba1ebe0fd89e2e2f2df45d8acac19b0d625
Roman Kiryanov <rkir@google.com>
2025-11-23 16:09:11 +0530

    RMX2020: Set PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS to false
    We support OTA - suppress the build warning.
    
    Bug: 295259752
    Test: presubmit
    Change-Id: I580703b8cb87578d644d10690c52561090fdabd2
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Signed-off-by: mikoker <mikolaj.kolata@gmail.com>

--- device.mk
+++ device.mk
@@ -141,6 +141,7 @@ $(call soong_config_set,libinit,vendor_init_lib,//$(DEVICE_PATH):libinit_RMX2020
 # Kernel
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_ENABLE_UFFD_GC := true
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
 
 # Keymaster
 PRODUCT_PACKAGES += \
