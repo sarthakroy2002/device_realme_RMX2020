80c9a60d85459a7847dfbea9c5d62846bbadfef4
Koushik Dey <kousickdey3@gmail.com>
2026-01-22 00:20:23 +0530

    Revert "RMX2020: init: Enable config_avoidGfxAccel for 3GB variants"
    This reverts commit c24ce5832a9cdab789a4237a2e34339bbc0f9f2d.
    
    * It disabled Blur on 3GB variants.
    
    Change-Id: I7d8d1fd8b1f76f9130eca910d86c7d259168fb7d

--- libinit/init_RMX2020.cpp
+++ libinit/init_RMX2020.cpp
@@ -115,18 +115,7 @@ void init_operator_name_properties() {
     }
 }
 
-void set_avoid_gfxaccel_config() {
-    struct sysinfo sys;
-    sysinfo(&sys);
-
-    if (sys.totalram <= 3072ull * 1024 * 1024) {
-        // Reduce memory footprint
-        property_override("ro.config.avoid_gfx_accel", "true");
-    }
-}
-
 void vendor_load_properties() {
-    set_avoid_gfxaccel_config();
     init_operator_name_properties();
 #ifdef __ANDROID_RECOVERY__
     std::string buildtype = GetProperty("ro.build.type", "userdebug");
