78e64877131a41212e1dc9a36d3152d28768dcc3
Mashopy <eliasgheeraert@gmail.com>
2026-02-24 12:26:20 +0100

    RMX2020: Inherit telephony-common-stub
    Google deprecated TelephonyMetrics class in Android 16 QPR2 which our IMS stack depends on.
    
    Change-Id: Idaaf1270e86ca9297a2278c2032d88f6352341cb

--- device.mk
+++ device.mk
@@ -131,6 +131,7 @@ PRODUCT_PACKAGES += \
     vndservicemanager
 
 # IMS
+$(call inherit-product, hardware/lineage/compat/frameworks/compat.mk)
 $(call inherit-product, hardware/mediatek/frameworks/mediatek-frameworks.mk)
 
 PRODUCT_COPY_FILES += \
