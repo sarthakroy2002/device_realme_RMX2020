e362d755cb2c06f72d19446b867e7f38afb0781c
Mashopy <eliasgheeraert@gmail.com>
2026-03-30 03:15:19 +0200

    RMX2020: Rename memtrack service to the new impl
    Change-Id: I50e1ac6bda22a4c0895f9fec785b9af1d0f81ad5

--- device.mk
+++ device.mk
@@ -80,7 +80,7 @@ PRODUCT_PACKAGES += \
     libvulkan
 
 PRODUCT_PACKAGES += \
-    android.hardware.memtrack-service.mediatek-mali
+    android.hardware.memtrack-service.mediatek
 
 # Doze
 PRODUCT_PACKAGES += \
